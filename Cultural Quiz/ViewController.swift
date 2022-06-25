3c5499b093a1d418ebb857be39bdcd23438c2233
dyahputrin <dyah.nariswari@tokopedia.com>
2022-06-25 11:27:51 +0700

    initial commit

---
+++ Cultural Quiz/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Cultural Quiz
+//
+//  Created by dyah.nariswari on 25/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
